9327422bb482b73c14bb391bc49ded5d98388ec7
Isabella <isabellapierce93@gmail.com>
2022-10-03 16:00:28 -0700

    completed JS logic for generating password

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-// Variables for lower, upper, numeric, special characters
-
+// Variables for lower, upper, numeric & special characters
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "0123456789";
 var symbols = "!@#$%^&*_-+=";
 
-// Array for password criteria
-var passwordCriteria = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!@#$%^&*_-+="];
-
-// Prompts for user input
-// var userChoice1 = window.prompt("Lowercase Letters? Yes or No: ");
-// var userChoice2 = window.prompt("Uppercase Letters? Yes or No: ");
-// var userChoice3 = window.prompt("Numbers? Yes or No: ");
-// var userChoice4 = window.prompt("Special Characters? Yes or No: ");
-// var userChoice5 = window.prompt("Enter character length between 8-128");
-
-// Add function for user input
-function generatePassword () {
-  var length = window.prompt("Select characters between 8-128:");
-  if (!length) {
-    alert("Please select a value.");
-  } else if (length > 8 || length < 128); {
-    alert("Password length must be between 8 and 128 characters");
-  } if (length < 8 || length > 128) {
-    
-  }
-}
-
-
 // Write password to the #password input
-var writePassword = function() {
+var writePassword = function () {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -41,20 +16,55 @@ var writePassword = function() {
 
 }
 
+// Function for user input to generate password
+function generatePassword() {
+  var password = "";
+  var passwordCrit = "";
+  // Ask for length of password
+  var length = window.prompt("Enter character length between 8-128");
+  if (length < 8 || length > 128) {
+  window.alert("Please enter a number between 8-128");
+    return;
+  } 
+  else {
+    // Ask for alpha, numeric & symbol criteria
+    var lower = window.confirm("If you want to include lowercase letters - Press 'Ok'.");
+    var upper = window.confirm("If you want to include uppercase letters - Press 'Ok'.");
+    var num = window.confirm("If you want to include numbers - Press 'Ok'.");
+    var sym = window.confirm("If you want to include special characters - Press 'Ok'.");
+    // If no user input was selected
+   if (!lower && !upper && !num && !sym){
+    window.alert("At least 1 criteria needs to be selected.");
+    return;
+   } 
+  // User says Ok to lowercase characters
+   if (lower){
+    passwordCrit = passwordCrit+(lowerCase);
+   }
+  // User says Ok to uppercase characters 
+   if (upper){
+    passwordCrit = passwordCrit+(upperCase);
+   }
+  // User says Ok to numbers
+   if (num){
+    passwordCrit = passwordCrit+(numeric);
+   }
+  // User says Ok to symbols
+   if (sym){
+    passwordCrit = passwordCrit+(symbols);
+   }
+
+  // loop to select password based on user input.
+  for (var i = 0; i < length; i++) {
+    var index = Math.floor(Math.random() * passwordCrit.length);
+    password = password + passwordCrit[index];
+  }
+  // Generate password
+  return (password);
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 writePassword()
-
-
-
-//array methods to store all details for gen password
-//start with writing out logic for password generator
-
-// promts for password criteria
-// length of character prompt 8 - 128 characters
-// character types prompt - lowercase, uppercase, numeric, and or special characters
-
-
-// have 4 variables for the criteria
-
