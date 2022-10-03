2b8e25bd2c2d50d6fe7f3c5a28f139d133cc6ac9
Isabella Pettini <isabellapettini@Isabellas-MBP.localdomain>
2022-10-02 19:12:07 -0700

    worked on JS for project

--- Develop/script.js
+++ Develop/script.js
@@ -13,11 +13,24 @@ var symbols = "!@#$%^&*_-+=";
 var passwordCriteria = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!@#$%^&*_-+="];
 
 // Prompts for user input
-var userChoice1 = window.prompt("Lowercase Letters? Yes or No: ");
-var userChoice2 = window.prompt("Uppercase Letters? Yes or No: ");
-var userChoice3 = window.prompt("Numbers? Yes or No: ");
-var userChoice4 = window.prompt("Special Characters? Yes or No: ");
-var userChoice5 = window.prompt("Enter character length between 8-128");
+// var userChoice1 = window.prompt("Lowercase Letters? Yes or No: ");
+// var userChoice2 = window.prompt("Uppercase Letters? Yes or No: ");
+// var userChoice3 = window.prompt("Numbers? Yes or No: ");
+// var userChoice4 = window.prompt("Special Characters? Yes or No: ");
+// var userChoice5 = window.prompt("Enter character length between 8-128");
+
+// Add function for user input
+function generatePassword () {
+  var length = window.prompt("Select characters between 8-128:");
+  if (!length) {
+    alert("Please select a value.");
+  } else if (length > 8 || length < 128); {
+    alert("Password length must be between 8 and 128 characters");
+  } if (length < 8 || length > 128) {
+    
+  }
+}
+
 
 // Write password to the #password input
 var writePassword = function() {
